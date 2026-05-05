1d92db97d78733a68b0f71a8414c25bac6743b5c
iTired <itired@music.com>
2026-05-05 19:51:12 +0400

    Fix run.py - remove async_mode from socketio.run()

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ sys.path.insert(0, 'app')
 from app import app, socketio
 
 if __name__ == '__main__':
-    print("Starting iTired server with gevent...")
-    socketio.run(app, host='0.0.0.0', port=5001, async_mode='gevent')
+    print("Starting iTired server...")
+    socketio.run(app, host='0.0.0.0', port=5001)
